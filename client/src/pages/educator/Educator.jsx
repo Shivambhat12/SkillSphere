a87539fbfc96d915df8d3d1a9a6612b66dc2b232
Shivambhat12 <shivam4bhatnagar@gmail.com>
2025-03-22 03:33:57 +0530

    educator dashboard initial component added

--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Navbar from "../../components/educator/Navbar";
+import Sidebar from "../../components/educator/Sidebar";
+import Footer from "../../components/educator/Footer";
 function Educator() {
   return (
-    <div>
-      <h1>Educator</h1>
-      <div>{<Outlet />}</div>
+    <div className="text default min-h-screen bg-white">
+      <Navbar />
+      <div className="flex">
+        <Sidebar />
+        {/* outlet is the place where child routes will be rendered */}
+        <div className="flex-1">{<Outlet />}</div>
+      </div>
+      <Footer />
     </div>
   );
 }
