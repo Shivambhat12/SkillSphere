af485bf53fd92eb179801d56fc7446a3532837b4
Shivambhat12 <shivam4bhatnagar@gmail.com>
2025-03-22 01:51:23 +0530

    student pages completed

--- client/src/components/students/Rating.jsx
+++ client/src/components/students/Rating.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-function Rating() {
+function Rating({ intitialRating, onRate }) {
+  const [rating, setRating] = useState(intitialRating || 0);
+
+  const handleRating = (value) => {
+    setRating(value);
+    if (onRate) onRate(value);
+  };
+
+  //  when the initial rating is changed than this function will execute
+  useEffect(() => {
+    if (intitialRating) {
+      setRating(intitialRating);
+    }
+  }, [intitialRating]);
   return (
     <div>
-      <h1>Rating</h1>
+      {Array.from({ length: 5 }, (_, index) => {
+        const starValue = index + 1;
+        return (
+          <span
+            key={index}
+            className={`text-xl sm:text-2xl cursor-pointer transition-colors ${
+              starValue <= rating ? "text-yellow-500" : "text-gray-400"
+            }`}
+            onClick={() => handleRating(starValue)}
+          >
+            &#9733;
+          </span>
+        );
+      })}
     </div>
   );
 }
--- client/src/pages/student/Player.jsx
+++ client/src/pages/student/Player.jsx
@@ -3,11 +3,19 @@ import { AppContext } from "../../context/AppContext";
 import { useParams } from "react-router-dom";
 import { assets } from "../../assets/assets";
 import humanizeDuration from "humanize-duration";
+// used to play the video using youtube tag
+import YouTube from "react-youtube";
+import Footer from "../../components/students/Footer";
+import Rating from "../../components/students/Rating";
 
 function Player() {
+  // here we are getting all enrolled courses from the context api
   const { enrolledCourses, calculateChapterTime } = useContext(AppContext);
+  // this will take the course id from the url useParams is builtin in the react router dom
   const { courseId } = useParams();
+  // state used to hold the reference of the particular course clicked by the user
   const [courseData, setCourseData] = useState(null);
+  // this state is used to track the opening of the drop down menu
   const [openSections, setopenSections] = useState({});
   const [playerData, setPlayerData] = useState(null);
 
@@ -18,99 +26,128 @@ function Player() {
       }
     });
   };
-  const toggleSection = (index) => {
-    setopenSections((prev) => ({ ...prev, [index]: !prev[index] }));
-  };
 
+  // using this hook the getCourseData function will execute as this compenent is mounted
   useEffect(() => {
     getCourseData();
   }, [enrolledCourses]);
-
+  //  function to set the state of the openSections and is copied from the courseDetails page
+  const toggleSection = (index) => {
+    setopenSections((prev) => ({ ...prev, [index]: !prev[index] }));
+  };
   return (
     <>
       <div className="p-4 sm:p-10 flex flex-col-reverse md:grid md:grid-cols-2 gap-10 md:px-36">
         {/* left column */}
-        <div className="text-gray-800"></div>
-        <h2 className="text-xl font-semibold">Course Structure</h2>
+        <div className="text-gray-800">
+          <h2 className="text-xl font-semibold">Course Structure</h2>
 
-        <div className="pt-5">
-          {courseData &&
-            courseData.courseContent.map((chapter, index) => (
-              <div
-                key={index}
-                className="border border-gray-300 bg-white mb-2 rounded"
-              >
+          <div className="pt-5">
+            {/* if we have the course data than only we will execute this line */}
+            {courseData &&
+              courseData.courseContent.map((chapter, index) => (
                 <div
-                  className="flex items-center justify-between px-4 py-3 cursor-pointer select-none"
-                  onClick={() => toggleSection(index)}
+                  key={index}
+                  className="border border-gray-300 bg-white mb-2 rounded"
                 >
-                  <div className="flex items-center gap-2">
-                    <img
-                      className={`transform transition-transform ${
-                        openSections[index] ? "rotate-180" : ""
-                      }`}
-                      src={assets.down_arrow_icon}
-                      alt="arrow icon"
-                    />
-                    <p className="font-medium md:text-base text-sm">
-                      {chapter.chapterTitle}
+                  <div
+                    className="flex items-center justify-between px-4 py-3 cursor-pointer select-none"
+                    onClick={() => toggleSection(index)}
+                  >
+                    <div className="flex items-center gap-2">
+                      <img
+                        className={`transform transition-transform ${
+                          openSections[index] ? "rotate-180" : ""
+                        }`}
+                        src={assets.down_arrow_icon}
+                        alt="arrow icon"
+                      />
+                      <p className="font-medium md:text-base text-sm">
+                        {chapter.chapterTitle}
+                      </p>
+                    </div>
+                    <p className="text-sm md:text-default">
+                      {chapter.chapterContent.lenght} lectures -{" "}
+                      {calculateChapterTime(chapter)}
                     </p>
                   </div>
-                  <p className="text-sm md:text-default">
-                    {chapter.chapterContent.lenght} lectures -{" "}
-                    {calculateChapterTime(chapter)}
-                  </p>
-                </div>
-                {/* adding ternary operator to add the funcitonality of the drop down menu */}
-                <div
-                  className={`overflow-hidden transition-all duration-300 ${
-                    openSections[index] ? "max-h-96" : "max-h-0"
-                  }`}
-                >
-                  <ul className="list-disc md:pl-10 pl-4 pr-2 text-gray-600 border-t border-gray-300">
-                    {chapter.chapterContent.map((lecture, i) => (
-                      <li key={i} className="flex items-start gap-2 py-1">
-                        <img
-                          src={false ? assets.blue_tick_icon : assets.play_icon}
-                          alt="play icon"
-                          className="w-4 h-4 mt-1"
-                        />
-                        <div className="flex items-center justify-between w-full text-gray-800 text-xs md:text-default">
-                          <p>{lecture.lectureTitle}</p>
-                          <div className="flex gap-2">
-                            {lecture.lectureUrl && (
-                              <p
-                                onClick={() =>
-                                  setPlayerData({
-                                    ...lecture,
-                                    chapter: index + 1,
-                                    lecture: i + 1,
-                                  })
-                                }
-                                className="text-blue-500 cursor-pointer"
-                              >
-                                Watch
-                              </p>
-                            )}
-                            <p>
-                              {humanizeDuration(
-                                lecture.lectureDuration * 60 * 1000,
-                                { units: ["h", "m"] }
+                  {/* adding ternary operator to add the funcitonality of the drop down menu */}
+                  <div
+                    className={`overflow-hidden transition-all duration-300 ${
+                      openSections[index] ? "max-h-96" : "max-h-0"
+                    }`}
+                  >
+                    <ul className="list-disc md:pl-10 pl-4 pr-2 text-gray-600 border-t border-gray-300">
+                      {chapter.chapterContent.map((lecture, i) => (
+                        <li key={i} className="flex items-start gap-2 py-1">
+                          <img
+                            src={
+                              false ? assets.blue_tick_icon : assets.play_icon
+                            }
+                            alt="play icon"
+                            className="w-4 h-4 mt-1"
+                          />
+                          <div className="flex items-center justify-between w-full text-gray-800 text-xs md:text-default">
+                            <p>{lecture.lectureTitle}</p>
+                            <div className="flex gap-2">
+                              {lecture.lectureUrl && (
+                                <p
+                                  onClick={() =>
+                                    setPlayerData({
+                                      ...lecture,
+                                      chapter: index + 1,
+                                      lecture: i + 1,
+                                    })
+                                  }
+                                  className="text-blue-500 cursor-pointer"
+                                >
+                                  Watch
+                                </p>
                               )}
-                            </p>
+                              <p>
+                                {humanizeDuration(
+                                  lecture.lectureDuration * 60 * 1000,
+                                  { units: ["h", "m"] }
+                                )}
+                              </p>
+                            </div>
                           </div>
-                        </div>
-                      </li>
-                    ))}
-                  </ul>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
                 </div>
+              ))}
+          </div>
+          <div className="flex items-center gap-2 py-3 mt-10">
+            <h1 className="text-xl font-bold">Rate this Course</h1>
+            <Rating intitialRating={0} />
+          </div>
+        </div>
+        {/* right column */}
+        <div className="md:mt-10">
+          {playerData ? (
+            <div>
+              <YouTube
+                videoId={playerData.lectureUrl.split("/").pop()}
+                iframeClassName="w-full aspect-video"
+              />
+              <div className="flex justify-between items-center mt-1">
+                <p>
+                  {playerData.chapter}.{playerData.lecture}{" "}
+                  {playerData.lectureTitle}
+                </p>
+                <button className="text-blue-600">
+                  {false ? "Completed" : "Mark Complete"}
+                </button>
               </div>
-            ))}
+            </div>
+          ) : (
+            <img src={courseData ? courseData.courseThumbnail : ""} alt="" />
+          )}
         </div>
       </div>
-
-      {/* right column */}
-      <div></div>
+      <Footer />
     </>
   );
 }
