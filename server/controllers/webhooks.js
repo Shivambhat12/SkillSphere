00d66435552cce875bc820ffa6975330014fe66c
Shivambhat12 <shivam4bhatnagar@gmail.com>
2025-03-23 17:40:26 +0530

    webhook again corrected

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -7,7 +7,7 @@ const clerkWebhooks = async (req, res) => {
     await whook.verify(JSON.stringify(req.body), {
       "svix-id": req.headers["svix-id"],
       "svix-timestamp": req.headers["svix-timestamp"],
-      "svix-signature": req - headers["svix-signature"],
+      "svix-signature": req.headers["svix-signature"],
     });
     const { data, type } = req.body;
     switch (type) {
@@ -24,7 +24,7 @@ const clerkWebhooks = async (req, res) => {
       }
       case "user.updated": {
         const userData = {
-          email: data.email_address[0].email_address,
+          email: data.email_addresses[0].email_address,
           name: data.first_name + " " + data.last_name,
           imageUrl: data.image_url,
         };
