f5a3278431ea768d9be13c5cf56e9aa48c3aa1ea
Shivambhat12 <shivam4bhatnagar@gmail.com>
2025-03-23 15:11:14 +0530

    first backend commit

--- server/server.js
+++ server/server.js
@@ -1,12 +1,23 @@
 import express from "express";
+
 // cors is used so that our backend can get connected with ant domain
 import cors from "cors";
+
 // for .env file
 import "dotenv/config";
+
+// getting the connectdb function from the config folder
+import connectDB from "./configs/mongodb.js";
+
 // creating the instance app of the express
 const app = express();
+
+// connectiong to the database
+await connectDB();
+
 // middleware to use the cors
 app.use(cors());
+
 // server will be listening on this port
 const PORT = process.env.PORT || 5000;
 
